8e24c2c31519f3c314c2a46105bd975aa0883800
Amber Adamson <amber.renee.adamson@gmail.com>
2023-05-11 18:19:45 -0400

    This is the second commit. I added some data to test output for the CSV file.

--- project.py
+++ project.py
@@ -8,16 +8,10 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 #function to convert to csv file
-def convert_to_csv(txt_file, csv_file):
-    with open(txt_file, 'r') as file:
-        lines = file.readlines()
-
+def convert_to_csv(dataset, csv_file):
     with open(csv_file, 'w', newline='') as file:
         writer = csv.writer(file)
-        for line in lines:
-            
-            row = line.strip().split(',')
-            writer.writerow(row)
+        writer.writerows(dataset)
 
 #function to send email
 def send_email(sender_email, sender_password, receiver_email, subject, body, attachment_file):
@@ -43,17 +37,29 @@ def send_email(sender_email, sender_password, receiver_email, subject, body, att
     server.quit()
 
 # placeholder details (to be modified)
-txt_file_path = 'input.txt'
-csv_file_path = 'output.csv'
-attachment_file_path = csv_file_path
+dataset = [
+    ['Name', 'Age', 'City'],
+    ['John Doe', '25', 'New York'],
+    ['Jane Smith', '30', 'Los Angeles'],
+    ['Mike Johnson', '40', 'Chicago']
+]
+
+# Placeholder details (to be modified)
 sender_email = 'amber.renee.adamson@gmail.com'
-sender_password = '[savedinfile]'
+sender_password = 'jqpdqjsyxvugozle'
 receiver_email = 'amber.renee.adamson@gmail.com'
 subject = 'CSV file'
 body = 'Please find the attached CSV file.'
 
-# call convert function
-convert_to_csv(txt_file_path, csv_file_path)
+# for loop for data
+accumulated_data = []
+for data in dataset:
+    accumulated_data.append(data)
 
-# call send function to send the file
-send_email(sender_email, sender_password, receiver_email, subject, body, attachment_file_path)
+# generate csv file
+csv_file_path = 'output.csv'
+convert_to_csv(accumulated_data, csv_file_path)
+
+# send the csv file
+send_email(sender_email, sender_password, receiver_email, subject, body, csv_file_path)
+
